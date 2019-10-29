02a434a207074cd2481aed1a78e0aba796ed3b8f
Renan Favaro <renan_favaro123@hotmail.com>
2019-10-29 10:03:22 -0200

    metodos de lista

--- index.php
+++ index.php
@@ -2,11 +2,22 @@
 
 require_once("config.php");
 
-$renan = new Usuario();
+//carrega um usuario
+//$renan = new Usuario();
+//$renan->loadbyId(4);
+//echo $renan;
 
-$renan->loadbyId(4);
+//carrega uma lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $renan;
+//carrega uma lista de usuarios buscando pelo login
+//$search = usuario::search("jo");
+//echo json_encode($search);
 
+//carrega um usuario usando o login e senha
+$usuario = new Usuario();
+$usuario->login("renan","akzlk3");
+echo $usuario;
 
 ?>
